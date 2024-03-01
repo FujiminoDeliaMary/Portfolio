ea9af42f9a9ce2bf3d6952d674f7dfc3ba89e129
TSENE <marilynedelia@gmail.com>
2024-03-01 17:42:13 +0100

    portfolio sans photo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,14 +12,14 @@ const About = () =>{
              <img src={delia} alt="" className="about__profile" data-aos="fade-down-right"/>
             <div className="about__speaking" data-aos="zoom-out">
                 <p>Actuellement entrain de suivre une formation de <strong>développeur FullStack</strong> à la rochelle, je suis une étudiante de 18 ans qui se spécialise en <strong>technologies du web</strong> tels que Css, Js, Html et bien d'autres framework et outils de développement
-                    front-end et back-end. Pationnée depuis maintenant 4 ans, si je devais me décrire en une technologie, je serais probablement ReactJs, innovante , fiable, en évolution et pleine de surprises.
+                    <strong>front-end</strong> et <strong>back-end</strong>. Pationnée depuis maintenant 4 ans, si je devais me décrire en une technologie, je serais probablement ReactJs, innovante , fiable, en évolution et pleine de surprises.
                 </p>
                 <br></br>
                 <p>N'étant pas qu'une développeuse mais avant tout une humaine, je suis aussi très passionée de tout ce qui touche musique, cinéma, manga et mythologie grecque.
                     Hormis tout cela , je serais très enchantée de travailler avec vous si mon profil vous intéresse.
                 </p>
                  <div className="about__actions">
-                    <p className="about__contact"><Link to='/contact'>Me contacter</Link></p>
+                    <p className="about__contact"><Link to='#contact'>Me contacter</Link></p>
                    <p className="about__cv"> <a href="./MarilyneCV.pdf" download="CV.pdf">Télécharger mon CV</a></p>
                  </div>
              </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { BiLogoLinkedinSquare } from "react-icons/bi";
 import { FaPhoneAlt } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { MdLocationOn } from "react-icons/md";
 
 const Footer = () =>{
     return <footer className="footer" >
-        <p><a href="tel:0651358858" className="footer__tel"><FaPhoneAlt />(+33) 06 51 35 88 58</a></p>
-        <p><a href="mailto:marilynedelia@gmail.com" className="footer__email"><MdEmail />marilynedelia@gmail.com</a></p>
-        <p><MdLocationOn />La rochelle 17000</p>
+             <p className="footer__linkedin"><BiLogoLinkedinSquare size={25}/><a href="https://www.linkedin.com/in/delia-marilyne-tsene-4895a52a9/" target="_blank" rel="noopener noreferrer">Linkedin</a></p>
+             <p className="footer__location"><MdLocationOn size={25}/>La rochelle 17000</p>
+
+             <p><a href="tel:0651358858" className="footer__tel"><FaPhoneAlt/>   06 51 35 88 58</a></p>
+      
+   
     </footer>
 }
 
