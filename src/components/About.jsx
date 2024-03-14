7fe68420f604ed2bbddc8656bd2b105ef4556ba0
TSENE <marilynedelia@gmail.com>
2024-03-14 07:57:36 +0100

    ajout de la photo et changement du CV

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import delia from '../assets/TFK.jpg'
+import profil from '../assets/profil.jpg'
 import { Link } from "react-scroll";
 
 const About = () =>{
@@ -9,18 +9,16 @@ const About = () =>{
         <h2 className="about__title" data-aos="zoom-out">Mon histoire</h2>
 
         <div className="about__content">
-             <img src={delia} alt="" className="about__profile" data-aos="fade-down-right"/>
+             <img src={profil} alt="" className="about__profile" data-aos="fade-down-right"/>
             <div className="about__speaking" data-aos="zoom-out">
-                <p>Actuellement entrain de suivre une formation de <strong>développeur FullStack</strong> à la rochelle, je suis une étudiante de 18 ans qui se spécialise en <strong>technologies du web</strong> tels que Css, Js, Html et bien d'autres framework et outils de développement
-                    <strong>front-end</strong> et <strong>back-end</strong>. Passionnée depuis maintenant 4 ans, si je devais me décrire en une technologie, je serais probablement ReactJs, innovante , fiable, en évolution et pleine de surprises.
-                </p>
+            <p>Je suis actuellement étudiante à La Rochelle, en pleine formation pour devenir développeuse FullStack, et j'ai 18 ans. Mon domaine de spécialisation couvre les technologies du web, incluant le CSS, le JavaScript, le HTML, ainsi que divers frameworks et outils dédiés au développement front-end et back-end. Ma passion pour ce domaine remonte à quatre ans déjà. Si je devais me comparer à une technologie,
+                 je choisirais sans hésiter ReactJs pour son caractère innovant, sa fiabilité, sa capacité d'adaptation et son potentiel surprenant.</p>
                 <br></br>
-                <p>N'étant pas qu'une développeuse mais avant tout une humaine, je suis aussi très passionée de tout ce qui touche musique, cinéma, manga et mythologie grecque.
-                    Hormis tout cela , je serais très enchantée de travailler avec vous si mon profil vous intéresse.
-                </p>
+                <p>Au-delà de ma casquette de développeuse, je suis une personne aux intérêts variés, passionnée par la musique, le cinéma, les mangas et la mythologie grecque. Je serais ravie d'apporter ma contribution à votre équipe si mon profil retient votre attention.</p><br></br>
+                
                  <div className="about__actions">
                     <p className="about__contact"><Link to='#contact'>Me contacter</Link></p>
-                   <p className="about__cv"> <a href="./MarilyneCV.pdf" download="CV.pdf">Télécharger mon CV</a></p>
+                   <p className="about__cv"> <a href="./CVTSENE.pdf" download="CV.pdf">Télécharger mon CV</a></p>
                  </div>
              </div>
             
